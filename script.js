4ee6236e8b38318fa2e762b2d59ab3194e47c743
Marley Marques <101825738+LOIRINMF@users.noreply.github.com>
2023-02-09 14:35:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,468 @@
+function MudaTema() {
+  document.body.classList.toggle("OutraC");
+  
+ // var BtnT =document.getElementById("BT")
+ //cosole.log(BtnT)
+}
+
+function MaisSV() {
+  var Ponto =document.getElementById("PV");
+  var Sobre =document.getElementById("SV");
+  var Btn =document.getElementById("MV");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Sobre nos"
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√∆filiadøs Twø Brøthers°";
+  }
+}
+
+function MaisSA() {
+  var Ponto =document.getElementById("Pontos");
+  var Sobre =document.getElementById("Sobre");
+  var Btn =document.getElementById("Mais");
+  
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+  
+}
+function MaisSB() {
+  var Ponto =document.getElementById("PB");
+  var Sobre =document.getElementById("SB");
+  var Btn =document.getElementById("MB");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+function MaisSC() {
+  var Ponto =document.getElementById("PC");
+  var Sobre =document.getElementById("SC");
+  var Btn =document.getElementById("MC");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSD() {
+  var Ponto =document.getElementById("PD");
+  var Sobre =document.getElementById("SD");
+  var Btn =document.getElementById("MD");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSE() {
+  var Ponto =document.getElementById("PE");
+  var Sobre =document.getElementById("SE");
+  var Btn =document.getElementById("ME");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSF() {
+  var Ponto =document.getElementById("PF");
+  var Sobre =document.getElementById("SF");
+  var Btn =document.getElementById("MF");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSG() {
+  var Ponto =document.getElementById("PG");
+  var Sobre =document.getElementById("SG");
+  var Btn =document.getElementById("MG");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSH() {
+  var Ponto =document.getElementById("PH");
+  var Sobre =document.getElementById("SH");
+  var Btn =document.getElementById("MH");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSI() {
+  var Ponto =document.getElementById("PI");
+  var Sobre =document.getElementById("SI");
+  var Btn =document.getElementById("MI");
+  
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSJ() {
+  var Ponto =document.getElementById("PJ");
+  var Sobre =document.getElementById("SJ");
+  var Btn =document.getElementById("MJ");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSK() {
+  var Ponto =document.getElementById("PK");
+  var Sobre =document.getElementById("SK");
+  var Btn =document.getElementById("MK");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSL() {
+  var Ponto =document.getElementById("PL");
+  var Sobre =document.getElementById("SL");
+  var Btn =document.getElementById("ML");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSM() {
+  var Ponto =document.getElementById("PM");
+  var Sobre =document.getElementById("SM");
+  var Btn =document.getElementById("MM");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSN() {
+  var Ponto =document.getElementById("PN");
+  var Sobre =document.getElementById("SN");
+  var Btn =document.getElementById("MN");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSO() {
+  var Ponto =document.getElementById("PO");
+  var Sobre =document.getElementById("SO");
+  var Btn =document.getElementById("MO");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSP() {
+  var Ponto =document.getElementById("PP");
+  var Sobre =document.getElementById("SP");
+  var Btn =document.getElementById("MP");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSQ() {
+  var Ponto =document.getElementById("PQ");
+  var Sobre =document.getElementById("SQ");
+  var Btn =document.getElementById("MQ");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSR() {
+  var Ponto =document.getElementById("PR");
+  var Sobre =document.getElementById("SR");
+  var Btn =document.getElementById("MR");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSS() {
+  var Ponto =document.getElementById("PS");
+  var Sobre =document.getElementById("SS");
+  var Btn =document.getElementById("MS");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisST() {
+  var Ponto =document.getElementById("PT");
+  var Sobre =document.getElementById("ST");
+  var Btn =document.getElementById("MT");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSU() {
+  var Ponto =document.getElementById("PU");
+  var Sobre =document.getElementById("SU");
+  var Btn =document.getElementById("MU");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSAA() {
+  var Ponto =document.getElementById("PAA");
+  var Sobre =document.getElementById("SAA");
+  var Btn =document.getElementById("MAA");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSAB() {
+  var Ponto =document.getElementById("PAB");
+  var Sobre =document.getElementById("SAB");
+  var Btn =document.getElementById("MAB");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSAC() {
+  var Ponto =document.getElementById("PAC");
+  var Sobre =document.getElementById("SAC");
+  var Btn =document.getElementById("MAC");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSAD() {
+  var Ponto =document.getElementById("PAD");
+  var Sobre =document.getElementById("SAD");
+  var Btn =document.getElementById("MAD");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSAE() {
+  var Ponto =document.getElementById("PAE");
+  var Sobre =document.getElementById("SAE");
+  var Btn =document.getElementById("MAE");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+
+function MaisSAF() {
+  var Ponto =document.getElementById("PAF");
+  var Sobre =document.getElementById("SAF");
+  var Btn =document.getElementById("MAF");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
+function MaisSAG() {
+  var Ponto =document.getElementById("PAG");
+  var Sobre =document.getElementById("SAG");
+  var Btn =document.getElementById("MAG");
+
+  if(Ponto.style.display === "none"){
+    Ponto.style.display = "inline";
+    Sobre.style.display = "none";
+    Btn.innerHTML = "√Mais sobre";
+  }else{
+    Ponto.style.display = "none";
+    Sobre.style.display = "inline";
+    Btn.innerHTML = "√Menos sobre";
+  }
+}
